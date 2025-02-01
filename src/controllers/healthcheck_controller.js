845f42005ede9352203b01eb2ec58ffc23cc8ef1
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2025-02-01 18:28:20 +0530

    Update healthcheck_controller.js

--- src/controllers/healthcheck_controller.js
+++ src/controllers/healthcheck_controller.js
@@ -6,14 +6,7 @@ import { asynchandler } from "../utils/asynchandler.js";
 
 //CREATING A HEALTH CHECK
 
-// const healthcheck =async(req,res)=>{//making it async because of database connection
-//     //there's no guarantee that the connection will happen everytime so we have to use try catch
-//     try {
-//         res.status(200).json
-//     } catch (error) {
-//     }
-// }
-//But we don't want to do try and catch all the time so we'll use the async handler already being built up
+
 
 //the main use of async handler is to wrap everything in a promise so we can handle errors in a beterr way
 
